5fe5823e03a612137267c252dfe0b8637f1c35fa
Jean-Marc Pouchoulon <jean-marc.pouchoulon@umontpellier.fr>
2022-02-17 10:58:08 +0100

    on cache kibana et evebox en 127.0.0.1

--- lance-siem.sh
+++ lance-siem.sh
@@ -45,12 +45,13 @@ suricata-update enable-source sslbl/ja3-fingerprints
 suricata-update enable-source ptresearch/attackdetection
 suricata-update
 '
-docker run --net evebox -d --net=host -it jasonish/evebox:latest -e http://127.0.0.1:9200 -i 'logstash*'
+docker run --net elasticsearch -d --publish=127.0.0.1:5636:5636 -it jasonish/evebox:latest -e http://elasticsearch:9200 -i 'logstash*'
 
 
-docker run -d --name kibana --network=elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:8.0.0 
+docker run -d --name kibana --network=elasticsearch --publish=127.0.0.1:5601:5601 docker.elastic.co/kibana/kibana:8.0.0 
 sleep 60
 docker run -d --name logstash -e PUID=$(id -u)  -e PGID=$(id -g)  -e XPACK_MONITORING_ENABLED=false -it --rm --net=elasticsearch --volumes-from=suricata -v $(pwd)/pipeline/:/usr/share/logstash/pipeline/ logstash:8.0.0
+
 docker run -d --name filebeat --user=root --network=elasticsearch  --volume="$(pwd)/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro" --volume="$(pwd)/suricata.yml:/usr/share/filebeat/modules.d/suricata.yml:ro"  --volume=$(pwd)/logs:/var/log/suricata docker.elastic.co/beats/filebeat:8.0.0 filebeat -e -strict.perms=false -E setup.kibana.host=kibana:5601 -E output.elasticsearch.hosts="elasticsearch:9200"
 docker exec -it filebeat filebeat setup -E setup.kibana.host=kibana:5601 -E output.elasticsearch.hosts="elasticsearch:9200"
 
