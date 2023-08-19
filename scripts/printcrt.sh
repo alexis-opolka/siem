040426cc7570316b3290f8b26e918fe929e5dc19
pushou <pushou@nowhere>
2023-08-19 23:39:56 +0200

    sauve

--- scripts/printcrt.sh
+++ scripts/printcrt.sh
@@ -1,8 +1,10 @@
 
-CRTKEY=$(openssl x509 -fingerprint -sha256 -in ./temp/ca.crt |grep -v Fingerprint|sed -e 's/^/\t/')
+#CRTKEY=$(openssl x509 -fingerprint -sha256 -in ./temp/ca.crt |grep -v Fingerprint|sed -e 's/^/\t/'|sed -e 's/^M//g')
+CRTKEY=$(openssl x509 -in ./temp/ca.crt)
+
 cat <<- EOF
 ssl:
   certificate_authorities:
-     - |
-      $CRTKEY
+    - |
+$(echo "$CRTKEY" | sed 's/^/      /')
 EOF
